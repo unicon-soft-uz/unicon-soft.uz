773f299e4d928265dd3b838fc167fb8ad8969fe3
Otabek_Vaxobov <elot2017@mail.ru>
2023-02-11 00:21:53 +0500

    feat: add reactstrap

--- components/Footer.js
+++ components/Footer.js
@@ -1,17 +1,54 @@
-export default function Footer() {
-    return(
-        <>
-        <div className="Footer">
-        Just like in threeJS OrbitControls need a reference to the camera and the renderers domElement,
-        we get the scenes camera and renderer by using the useThree()
-         hook which react-fiber provides, when working with controls in react fiber we need to first call the extend function, this
-          extends/adds additional features to react fiber in this case OrbitControls. Then the we need to add orbitControls component
-           and use the attach property in it to basically attach our OrbitControls to the scene (this will allow us to access the orbit 
-           Controls in any component), this is going to be very handy when we want to make our box draggable, we will need to disable orbit
-            controls when we drag an the box in our scene.
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from 'reactstrap';
 
-Once we done we need to import our OrbitControls to our scene... you index page should look like this
-        </div>
-        </>
-    )
+function Footer(args) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar {...args}>
+        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="me-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/">GitHub</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Option 1</DropdownItem>
+                <DropdownItem>Option 2</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Reset</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
 }
+
+export default Footer;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charset="UTF-8"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" type="image/png" sizes="32x32" src="/favicon-32x32" />
+        <link rel="icon" type="image/png" sizes="16x16" src="/favicon-16x16" />
+        <title>UNICON-SOFT</title>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
