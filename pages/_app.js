7f5ffe4e32cb2743b812065aeafc1cea6de2f8d4
Otabek_Vaxobov <elot2017@mail.ru>
2023-02-12 23:05:06 +0500

    feat: add header and fix some things

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import 'tailwindcss/tailwind.css';
 import '../styles/globals.css';
 import { ThemeProvider } from 'next-themes';
 import { Layout } from '../sections/Layout';
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
--- sections/Header.js
+++ sections/Header.js
@@ -1,49 +1,335 @@
 import Link from 'next/link';
+import { Logo } from '../components/Logo';
 import { useState, useEffect } from 'react';
 import { useTheme } from 'next-themes';
-import { Logo } from '../components/Logo';
-import { SunIcon, MoonIcon } from '@heroicons/react/solid';
+import { Fragment } from 'react';
+import { Popover, Transition } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import { ChevronDownIcon, SunIcon, MoonIcon } from '@heroicons/react/20/solid';
+import {
+  solutions,
+  callsToAction,
+  recentPosts,
+  resources,
+} from '../components/exports';
 
-function Header() {
-  const { systemTheme, theme, setTheme } = useTheme();
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
+
+function ThemeChanger() {
   const [mounted, setMounted] = useState(false);
-
   useEffect(() => {
     setMounted(true);
   }, []);
-  const renderThemeChanger = () => {
-    if (!mounted) return null;
+  const { systemTheme, theme, setTheme } = useTheme();
+  if (!mounted) return null;
 
-    const currentTheme = theme === 'system' ? systemTheme : theme;
-
-    if (currentTheme === 'dark') {
-      return (
-        <SunIcon
-          className="w-10 h-10 text-yellow-500 "
-          role="button"
-          onClick={() => setTheme('light')}
-        />
-      );
-    } else {
-      return (
-        <MoonIcon
-          className="w-10 h-10 text-indigo-500 "
-          role="button"
-          onClick={() => setTheme('dark')}
-        />
-      );
-    }
-  };
-
+  const currentTheme = theme === 'system' ? systemTheme : theme;
+  return currentTheme === 'dark' ? (
+    <SunIcon
+      className="w-10 h-10 text-yellow-500 "
+      role="button"
+      onClick={() => setTheme('light')}
+    />
+  ) : (
+    <MoonIcon
+      className="w-10 h-10 text-indigo-500 "
+      role="button"
+      onClick={() => setTheme('dark')}
+    />
+  );
+}
+export default function Header() {
+  // if (currentTheme === 'dark') {
+  //   return (
+  //     <Sun
+  //       className="w-10 h-10 text-yellow-500 "
+  //       role="button"
+  //       onClick={() => setTheme('light')}
+  //     />
+  //   );
+  // } else {
+  //   return (
+  //     <FaMoon
+  //       className="w-10 h-10 text-indigo-500 "
+  //       role="button"
+  //       onClick={() => setTheme('dark')}
+  //     />
+  //   );
+  // }
+  //}
   return (
-    <header className="h-15 shadow-sm dark:border-gray-700">
-      <div className="container  px-4 sm:px-6 py-4 flex justify-between items-center">
-        <Logo />
+    <header>
+      <Popover className="relative bg-white z-50">
+        <div className="mx-auto max-w-7xl px-6">
+          <div className="flex items-center justify-between border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+            <div className="flex justify-start lg:w-0 lg:flex-1">
+              <Logo />
+            </div>
+            <div className="-my-2 -mr-2 md:hidden">
+              <Popover.Button className=" z-50 inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                <span className="sr-only">Open menu</span>
+                <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+              </Popover.Button>
+            </div>
+            <Popover.Group as="nav" className="hidden space-x-10 md:flex">
+              <Popover className="relative">
+                {({ open }) => (
+                  <>
+                    <Popover.Button
+                      className={classNames(
+                        open ? 'text-gray-900' : 'text-gray-500',
+                        'group inline-flex items-center rounded-md bg-white text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
+                      )}
+                    >
+                      <span>Modullar</span>
+                      <ChevronDownIcon
+                        className={classNames(
+                          open ? 'text-gray-600' : 'text-gray-400',
+                          'ml-2 h-5 w-5 group-hover:text-gray-500'
+                        )}
+                        aria-hidden="true"
+                      />
+                    </Popover.Button>
 
-        {renderThemeChanger()}
-      </div>
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-200"
+                      enterFrom="opacity-0 translate-y-1"
+                      enterTo="opacity-100 translate-y-0"
+                      leave="transition ease-in duration-150"
+                      leaveFrom="opacity-100 translate-y-0"
+                      leaveTo="opacity-0 translate-y-1"
+                    >
+                      <Popover.Panel className="absolute z-10 -ml-4 mt-3 w-screen max-w-md transform px-2 sm:px-0 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2">
+                        <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                          <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                            {solutions.map((item) => (
+                              <a
+                                key={item.name}
+                                href={item.href}
+                                className="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-50"
+                              >
+                                <item.icon
+                                  className="h-6 w-6 flex-shrink-0 text-indigo-600"
+                                  aria-hidden="true"
+                                />
+                                <span className="ml-4">
+                                  <p className="text-base font-medium text-gray-900">
+                                    {item.name}
+                                  </p>
+                                  <p className="mt-1 text-sm text-gray-500">
+                                    {item.description}
+                                  </p>
+                                </span>
+                              </a>
+                            ))}
+                          </div>
+                          <div className="space-y-6 bg-gray-50 px-5 py-5 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
+                            {callsToAction.map((item) => (
+                              <div key={item.name} className="flow-root">
+                                <a
+                                  href={item.href}
+                                  className="-m-3 flex items-center rounded-md p-3 text-base font-medium text-gray-900 hover:bg-gray-100"
+                                >
+                                  <item.icon
+                                    className="h-6 w-6 flex-shrink-0 text-gray-400"
+                                    aria-hidden="true"
+                                  />
+                                  <span className="ml-3">{item.name}</span>
+                                </a>
+                              </div>
+                            ))}
+                          </div>
+                        </div>
+                      </Popover.Panel>
+                    </Transition>
+                  </>
+                )}
+              </Popover>
+
+              <Link
+                href="info"
+                className="text-base font-medium text-gray-500 hover:text-gray-900"
+              >
+                Biz haqimizda
+              </Link>
+              <a
+                href="docs"
+                className="text-base font-medium text-gray-500 hover:text-gray-900"
+              >
+                Qo&apos;llanma
+              </a>
+
+              <Popover className="relative z-50">
+                {({ open }) => (
+                  <>
+                    <Popover.Button
+                      className={classNames(
+                        open ? 'text-gray-900' : 'text-gray-500',
+                        'group inline-flex items-center rounded-md bg-white text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
+                      )}
+                    >
+                      <span>More</span>
+                      <ChevronDownIcon
+                        className={classNames(
+                          open ? 'text-gray-600' : 'text-gray-400',
+                          'ml-2 h-5 w-5 group-hover:text-gray-500'
+                        )}
+                        aria-hidden="true"
+                      />
+                    </Popover.Button>
+
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-200"
+                      enterFrom="opacity-0 translate-y-1"
+                      enterTo="opacity-100 translate-y-0"
+                      leave="transition ease-in duration-150"
+                      leaveFrom="opacity-100 translate-y-0"
+                      leaveTo="opacity-0 translate-y-1"
+                    >
+                      <Popover.Panel className="absolute left-1/2 z-1000000 mt-3 w-screen max-w-md -translate-x-1/2 transform px-2 sm:px-0">
+                        <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                          <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                            {resources.map((item) => (
+                              <a
+                                key={item.name}
+                                href={item.href}
+                                className="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-50"
+                              >
+                                <item.icon
+                                  className="h-6 w-6 flex-shrink-0 text-indigo-600"
+                                  aria-hidden="true"
+                                />
+                                <div className="ml-4">
+                                  <p className="text-base font-medium text-gray-900">
+                                    {item.name}
+                                  </p>
+                                  <p className="mt-1 text-sm text-gray-500">
+                                    {item.description}
+                                  </p>
+                                </div>
+                              </a>
+                            ))}
+                          </div>
+                          <div className="bg-gray-50 px-5 py-5 sm:px-8 sm:py-8">
+                            <div>
+                              <h3 className="text-base font-medium text-gray-500">
+                                Recent Posts
+                              </h3>
+                              <ul role="list" className="mt-4 space-y-4">
+                                {recentPosts.map((post) => (
+                                  <li
+                                    key={post.id}
+                                    className="truncate text-base"
+                                  >
+                                    <a
+                                      href={post.href}
+                                      className="font-medium text-gray-900 hover:text-gray-700"
+                                    >
+                                      {post.name}
+                                    </a>
+                                  </li>
+                                ))}
+                              </ul>
+                            </div>
+                            <div className="mt-5 text-sm">
+                              <a
+                                href="#"
+                                className="font-medium text-indigo-600 hover:text-indigo-500"
+                              >
+                                View all posts
+                                <span aria-hidden="true"> &rarr;</span>
+                              </a>
+                            </div>
+                          </div>
+                        </div>
+                      </Popover.Panel>
+                    </Transition>
+                  </>
+                )}
+              </Popover>
+            </Popover.Group>
+            <ThemeChanger />
+          </div>
+        </div>
+        <Transition
+          as={Fragment}
+          enter="duration-200 ease-out"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="duration-100 ease-in"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          <Popover.Panel
+            focus
+            className="absolute z-50 inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden"
+          >
+            <div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+              <div className="px-5 pt-5 pb-6">
+                <div className="flex items-center justify-between">
+                  <div>
+                    <Logo />
+                  </div>
+                  <div className="-mr-2">
+                    <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                      <span className="sr-only">Close menu</span>
+                      <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                    </Popover.Button>
+                  </div>
+                </div>
+                <div className="mt-6">
+                  <nav className="grid gap-y-8">
+                    {solutions.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className="-m-3 flex items-center rounded-md p-3 hover:bg-gray-50"
+                      >
+                        <item.icon
+                          className="h-6 w-6 flex-shrink-0 text-indigo-600"
+                          aria-hidden="true"
+                        />
+                        <span className="ml-3 text-base font-medium text-gray-900">
+                          {item.name}
+                        </span>
+                      </a>
+                    ))}
+                  </nav>
+                </div>
+              </div>
+              <div className="space-y-6 py-6 px-5">
+                <div className="grid grid-cols-2 gap-y-4 gap-x-8">
+                  <Link
+                    href="/info"
+                    className="text-base font-medium text-gray-900 hover:text-gray-700"
+                  >
+                    Info
+                  </Link>
+                  <a
+                    href="#"
+                    className="text-base font-medium text-gray-900 hover:text-gray-700"
+                  >
+                    Docs
+                  </a>
+                  {resources.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="text-base font-medium text-gray-900 hover:text-gray-700"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </Popover.Panel>
+        </Transition>
+      </Popover>
     </header>
   );
 }
-
-export default Header;
