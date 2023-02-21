36cf16e551d2fc30b7f605ea8ec494a042cc56f7
Otabek_Vaxobov <elot2017@mail.ru>
2023-02-22 00:04:53 +0500

    feat: fetch data from github

--- components/FetchData.js
+++ components/FetchData.js
@@ -1,20 +1,63 @@
-const url = `https://github.com/unicon-soft-uz/unicon-soft.uz/blob/development/data.json`;
-const url2 = `https://raw.githubusercontent.com/unicon-soft-uz/unicon-soft.uz/development/data.json`;
-// export async function getServerSideProps() {
-//   // // Fetch data from external API
-//   // const res = await fetch(url);
-//   // const data = await res.json();
+import Image from 'next/image';
+import Link from 'next/link';
+import { useState, useEffect } from 'react';
 
-//   // // Pass data to the page via props
-//   // return { props: { res } };
-//   const res = fetch(url2)
-//     .then((response) => response.json())
-//     .then((data) => console.log(data));
-// }
+const url = `https://raw.githubusercontent.com/unicon-soft-uz/data/main/data.json`;
 
 export default function Guyz() {
-  const res = fetch(url2)
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err));
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (!data) return <p>No profile data</p>;
+
+  return (
+    <>
+      <>
+        <div className=" py-24 sm:py-32">
+          <div className=" grid max-w-7xl gap-y-20 gap-x-8 px-6 lg:px-8 xl:grid-cols-1">
+            <ul
+              role="list"
+              className="grid gap-x-8 gap-y-12 sm:grid-cols-3 sm:gap-y-16 xl:col-span-2"
+            >
+              {data.jamoamiz.map((person) => (
+                <li key={person.id}>
+                  <div className="flex items-center gap-x-6">
+                    <Image
+                      width={50}
+                      height={50}
+                      className="h-16 w-16 rounded-full"
+                      src={person.photo}
+                      alt="alt"
+                    />
+                    <div>
+                      <h3 className="text-base font-semibold leading-7 tracking-tight">
+                        {person.name}
+                      </h3>
+                      <p className="text-sm font-semibold leading-6 text-indigo-600">
+                        {person.rol}
+                      </p>
+                      <p className="text-sm font-semibold leading-6">
+                        {person.birthday}
+                      </p>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </>
+    </>
+  );
 }
