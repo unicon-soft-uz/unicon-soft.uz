0c6e200ac18fbd7fd151f34bf842c68567369688
Otabek_Vaxobov <elot2017@mail.ru>
2023-04-27 21:30:41 +0500

    feat: add firebase

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,18 @@ import 'tailwindcss/tailwind.css';
 import '../styles/globals.css';
 import { ThemeProvider } from 'next-themes';
 import { Layout } from '../sections/Layout';
+import AuthProvider from '../context/AuthContext';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <>
-      <ThemeProvider enableSystem={true} attribute="class">
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ThemeProvider>
+      <AuthProvider>
+        <ThemeProvider enableSystem={true} attribute="class">
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ThemeProvider>
+      </AuthProvider>
     </>
   );
 }
